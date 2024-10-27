7ae992bc8ba452c2635a1137824c8210eef3d75b
jumagu <jmgg.gutierrez@gmail.com>
2024-10-27 13:07:07 -0500

    refactor: rename the state variables to keep the variable naming convention in the app

--- src/stores/palces.store.ts
+++ src/stores/palces.store.ts
@@ -5,52 +5,52 @@ import searchApi from '@/apis/search.api';
 import type { Feature, PlacesResponse } from '@/interfaces';
 
 export const usePlacesStore = defineStore('places', () => {
-  const places = ref<Feature[]>([]);
-  const isLoading = ref(true);
-  const isLoadingPlaces = ref(false);
-  const userLocation = ref<[number, number]>();
+  const _places = ref<Feature[]>([]);
+  const _isLoading = ref(true);
+  const _isLoadingPlaces = ref(false);
+  const _userLocation = ref<[number, number]>();
 
   const getUserLocation = () => {
     navigator.geolocation.getCurrentPosition(
       ({ coords }) => {
-        userLocation.value = [coords.longitude, coords.latitude];
-        isLoading.value = false;
+        _userLocation.value = [coords.longitude, coords.latitude];
+        _isLoading.value = false;
       },
       () => {
-        userLocation.value = undefined;
-        isLoading.value = false;
+        _userLocation.value = undefined;
+        _isLoading.value = false;
       },
     );
   };
 
   const searchPlacesByTerm = async (query: string) => {
-    if (!userLocation.value) throw new Error('No user location.');
-    if (query.trim().length === 0) return (places.value = []);
+    if (!_userLocation.value) throw new Error('No user location.');
+    if (query.trim().length === 0) return (_places.value = []);
 
-    isLoadingPlaces.value = true;
+    _isLoadingPlaces.value = true;
 
     try {
       const res = await searchApi.get<PlacesResponse>('', {
         params: {
           q: query,
-          proximity: userLocation.value?.join(','),
+          proximity: _userLocation.value?.join(','),
         },
       });
-      places.value = res.data.features;
+      _places.value = res.data.features;
     } catch (error) {
       console.error(error);
-      places.value = [];
+      _places.value = [];
     } finally {
-      isLoadingPlaces.value = false;
+      _isLoadingPlaces.value = false;
     }
   };
 
   return {
     // Getters
-    places: computed(() => places.value),
-    isLoading: computed(() => isLoading.value),
-    userLocation: computed(() => userLocation.value),
-    isLoadingPlaces: computed(() => isLoadingPlaces.value),
+    places: computed(() => _places.value),
+    isLoading: computed(() => _isLoading.value),
+    userLocation: computed(() => _userLocation.value),
+    isLoadingPlaces: computed(() => _isLoadingPlaces.value),
 
     // Actions
     getUserLocation,
